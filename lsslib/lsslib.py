0de9c5901e538871793c3573f42a6a01cfc273d9
nate-maxwell <natedmaxwell@gmail.com>
2026-04-25 12:28:59 -0700

    separated lib and command file

--- lsslib/lsslib.py
+++ lsslib/lsslib.py
@@ -221,29 +221,3 @@ def lss(path: str | os.PathLike, all_files: bool = False) -> None:
         return
     for i in others:
         print(i)
-
-
-def main() -> None:
-    parser = argparse.ArgumentParser(
-        prog="lss", description="List image sequences in a directory."
-    )
-    parser.add_argument(
-        "paths",
-        nargs="*",
-        default=["."],
-        help="Directories to list (default: current directory).",
-    )
-    parser.add_argument(
-        "-a", "--all", action="store_true", help="Also list non-sequence files."
-    )
-    args = parser.parse_args()
-
-    for path in args.paths:
-        if len(args.paths) > 1:
-            print(f"{path}:")
-
-        lss(path, args.all)
-
-
-if __name__ == "__main__":
-    main()
